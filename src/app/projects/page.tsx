6613cc573137b022fe961d4f49239e9772d9b8b3
Justin Siek <justinsiek05@gmail.com>
2025-02-05 18:39:17 -0800

    added borders to tetrominos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,9 +13,7 @@ const tetrominoes = [
       { row: 18, col: 6 },
       { row: 17, col: 6 },
       { row: 16, col: 6 }
-
     ]
-
   },
   {
     type: 'J',
@@ -25,7 +23,6 @@ const tetrominoes = [
       { row: 19, col: 5 },
       { row: 18, col: 5 }
     ]
-
   },
   {
     type: 'L',
@@ -35,7 +32,6 @@ const tetrominoes = [
       { row: 19, col: 9 },
       { row: 18, col: 7 }
     ]
-
   },
   {
     type: 'O',
@@ -44,10 +40,6 @@ const tetrominoes = [
       { row: 19, col: 1 },
       { row: 18, col: 0 },
       { row: 18, col: 1 }
-
-
-
-
     ]
   },
   {
@@ -66,7 +58,6 @@ const tetrominoes = [
       { row: 18, col: 4 },
       { row: 17, col: 4 },
       { row: 17, col: 5 }
-
     ]
   },
   {
@@ -85,27 +76,31 @@ const boardRows = 20
 const boardCols = 10
 
 const Projects = () => {
-
+  // These are the colors on hover for each tetromino type.
   const hoverColors: { [key: string]: string } = {
-    I: 'group-hover:bg-cyan-500',
-    J: 'group-hover:bg-blue-500',
-    L: 'group-hover:bg-orange-500',
-    O: 'group-hover:bg-yellow-500',
-    T: 'group-hover:bg-purple-500',
-    S: 'group-hover:bg-green-500',
-    Z: 'group-hover:bg-red-500'
+    I: 'group-hover:bg-cyan-400',
+    J: 'group-hover:bg-blue-400',
+    L: 'group-hover:bg-orange-400',
+    O: 'group-hover:bg-yellow-400',
+    T: 'group-hover:bg-purple-400',
+    S: 'group-hover:bg-green-400',
+    Z: 'group-hover:bg-red-400'
   }
 
   return (
     <div className="bg-black min-h-screen flex items-center justify-center p-5">
       {/* Board container with fixed dimensions */}
-      <div className="relative border border-white" style={{ width: boardCols * cellSize, height: boardRows * cellSize }}>
+      <div
+        className="relative border border-white"
+        style={{ width: boardCols * cellSize, height: boardRows * cellSize }}
+      >
         {/* Background layer: one continuous black rectangle */}
         <div className="absolute inset-0 bg-black"></div>
 
         {/* Tetromino overlay(s) */}
         {tetrominoes.map((tetromino, i) => {
           // Compute the bounding box of this tetromino.
+          // (This is still used for positioning the group container.)
           const rows = tetromino.positions.map(p => p.row)
           const cols = tetromino.positions.map(p => p.col)
           const minRow = Math.min(...rows)
@@ -117,6 +112,9 @@ const Projects = () => {
 
           const hoverClass = hoverColors[tetromino.type]
 
+          // Create a lookup for the cells in this tetromino.
+          const cellSet = new Set(tetromino.positions.map(p => `${p.row},${p.col}`))
+
           return (
             // The tetromino container is an absolutely positioned group.
             <div
@@ -124,18 +122,36 @@ const Projects = () => {
               className="group absolute"
               style={{ top: minRow * cellSize, left: minCol * cellSize, width, height }}
             >
-              {tetromino.positions.map((pos, j) => (
-                <div
-                  key={j}
-                  className={`absolute bg-white transition-colors duration-50 ${hoverClass}`}
-                  style={{
-                    top: (pos.row - minRow) * cellSize,
-                    left: (pos.col - minCol) * cellSize,
-                    width: cellSize,
-                    height: cellSize
-                  }}
-                />
-              ))}
+              {tetromino.positions.map((pos, j) => {
+                // Check if neighboring positions exist.
+                const hasTop = cellSet.has(`${pos.row - 1},${pos.col}`)
+                const hasBottom = cellSet.has(`${pos.row + 1},${pos.col}`)
+                const hasLeft = cellSet.has(`${pos.row},${pos.col - 1}`)
+                const hasRight = cellSet.has(`${pos.row},${pos.col + 1}`)
+
+                // Only add borders for the sides that are not adjacent to another cell in the tetromino.
+                const borderClasses = [
+                  !hasTop && 'border-t',
+                  !hasBottom && 'border-b',
+                  !hasLeft && 'border-l',
+                  !hasRight && 'border-r'
+                ]
+                  .filter(Boolean)
+                  .join(' ')
+
+                return (
+                  <div
+                    key={j}
+                    className={`absolute bg-white transition-colors ${hoverClass} ${borderClasses} border-black`}
+                    style={{
+                      top: (pos.row - minRow) * cellSize,
+                      left: (pos.col - minCol) * cellSize,
+                      width: cellSize,
+                      height: cellSize
+                    }}
+                  />
+                )
+              })}
             </div>
           )
         })}
